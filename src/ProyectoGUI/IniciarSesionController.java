67aa1ee1a11beec7e430ae12fa6dde86bd2f7b80
josanchezva <josanchezva@unal.edu.co>
2020-07-02 23:26:38 -0500

    Avance GUI 3 vistas, 2 funcionales

--- src/ProyectoGUI/IniciarSesionController.java
+++ src/ProyectoGUI/IniciarSesionController.java
@@ -16,7 +16,6 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
@@ -24,26 +23,48 @@ import proyecto.Avl_tree;
 import proyecto.Usuario;
 
 public class IniciarSesionController implements Initializable {
-    private Avl_tree<Usuario> usuarios1 = new Avl_tree<>();
+    private Avl_tree<Usuario> usuarios1;
     Usuario u1 = new Usuario(1515,"1515");
-    @FXML private Label labelusuario;
-    @FXML private Button iniciarsesion;
+
     @FXML private TextField textid;
     @FXML private TextField textcontraseña;
     @FXML private Label labelError;
-    
+    @FXML private Label alarmedica;
+    @FXML private Label title;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        /* id para css
+            titles : usar para labels que requieran un borde redondo de fondo blanco con padding de 5
+         */
+        usuarios1 = new Avl_tree<>();
+        title.setId("titles");
+        title.getStylesheets().add("stylesheets/specific.css");
+
+        alarmedica.setId("titles");
+        alarmedica.getStylesheets().add("stylesheets/specific.css");
+
+        labelError.setId("label-error");
+        labelError.getStylesheets().add("stylesheets/specific.css");
+
         usuarios1.insert(u1);
     }
-    public void iniciarSesion(){
+    public void iniciarSesion(ActionEvent event) throws IOException{
         labelError.setVisible(false);
         try {
             int id=Integer.parseInt(textid.getText());
-            String contraseña = textcontraseña.getText();
-            Usuario comprobar = new Usuario(id,contraseña);
+            String password = textcontraseña.getText();
+            Usuario comprobar = new Usuario(id,password);
             if(usuarios1.find(comprobar)== null){
                 labelError.setVisible(true);
+            }
+            else{
+                Parent registro = FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
+                Scene mainMenuScene= new Scene (registro);
+                mainMenuScene.getStylesheets().add("stylesheets/styles.css");
+                Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+                window.setScene(mainMenuScene);
+                window.show();
             }
         }
         catch(IllegalFormatException | NumberFormatException e){
@@ -53,8 +74,10 @@ public class IniciarSesionController implements Initializable {
     public void registrarse(ActionEvent event) throws IOException{
         Parent registro = FXMLLoader.load(getClass().getResource("Registro.fxml"));
         Scene registroScene= new Scene (registro);
+        registroScene.getStylesheets().add("stylesheets/styles.css");
         Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
         window.setScene(registroScene);
         window.show();
     }
+
 }
--- src/ProyectoGUI/RegistroController.java
+++ src/ProyectoGUI/RegistroController.java
@@ -5,9 +5,24 @@
  */
 package ProyectoGUI;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.IllegalFormatException;
 import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import proyecto.Avl_tree;
+import proyecto.Usuario;
 
 /**
  * FXML Controller class
@@ -16,14 +31,64 @@ import javafx.fxml.Initializable;
  */
 public class RegistroController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     * @param url
-     * @param rb
-     */
+
+
+    @FXML private Label title;
+    @FXML private Label labelError;
+    @FXML private TextField textFieldID;
+    @FXML private PasswordField passwordField;
+    @FXML private PasswordField passwordFieldVerification;
+    @FXML private Label alarmed;
+
+    private Avl_tree<Usuario> usuariosAvlTree;
+
+    /* id para CSS
+             titles : usar para labels que requieran un borde redondo de fondo blanco con padding de 5
+             label-error: usar para el envío del mensaje a ususario solo hay un label de este tipo,
+    */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+        usuariosAvlTree = new Avl_tree<>();
+        Usuario usuario =new Usuario(1515,"1515");
+        usuariosAvlTree.insert(usuario);
+        title.setId("titles");
+        title.getStylesheets().add("stylesheets/specific.css");
+        alarmed.setId("titles");
+        alarmed.getStylesheets().add("stylesheets/specific.css");
+        labelError.setId("label-error");
+        labelError.getStylesheets().add("stylesheets/specific.css");
+    }
+    public void crearUsuario(ActionEvent event) throws IOException{
+        labelError.setVisible(false);
+        try {
+            int id=Integer.parseInt(textFieldID.getText());
+            String password = passwordField.getText();
+            String passwordVerification = passwordFieldVerification.getText();
+            Usuario comprobar = new Usuario(id,password);
+            if(usuariosAvlTree.contains(comprobar)){ //verificar si se puede corroborar por id y no por el objeto completamente
+                labelError.setVisible(true);
+                labelError.setText("El usuario ya existe");
+            }
+            else if(!password.equals(passwordVerification)){
+                throw new IllegalArgumentException();
+            }
+            else{
+                Parent registro = FXMLLoader.load(getClass().getResource("IniciarSesion.fxml"));
+                Scene iniciarSesionScene= new Scene (registro);
+                iniciarSesionScene.getStylesheets().add("stylesheets/styles.css");
+                Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+                window.setScene(iniciarSesionScene);
+                window.show();
+            }
+        }
+        catch(IllegalFormatException | NumberFormatException e){
+            labelError.setVisible(true);
+            labelError.setText("Entrada no válida");
+        }
+        catch (IllegalArgumentException passwordMismatch){
+            labelError.setVisible(true);
+            labelError.setText("Las contraseñas no coinciden");
+        }
+    }
+
 }
